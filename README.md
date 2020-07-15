5383fad004d83de30aee122cdb492ce8aa660c85
Matt <matthewd102@hotmail.com>
2020-07-15 13:48:44 +0100

    Implement Removable Devices write blocking from Group Policy via Regedit

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
 # Windows Registry Write Blocker
 
 ## Disclaimer
-This software has been tested to work on a Windows 10 Home laptop with a Kingston 32GB USB. As Windows is constantly changing, it is highly important that you test this before use on any potential digital evidence. There are no express guarantees or warranties, both suggested or implied with this software.
+This software has been tested to work on a Windows 10 Home laptop with a [Kingston DataTraveller G4 32GB USB](https://www.amazon.co.uk/Kingston-DTIG4-32GB-Traveler-Flash/dp/B00G9WHN12/), and an [Inatek 2.5" USB Caddy](https://www.amazon.co.uk/Optimized-Inateck-3-0-Portable-2-5-SATA-I-Additional/dp/B00KW4T69A/). As Windows is constantly changing, it is highly important that you test this before use on any potential digital evidence. There are no express guarantees or warranties, both suggested or implied with this software.
 
 ## Installation
-Download the latest version from the Releases tab. Alternatively you can download the repository and compile it from source with Visual Studio 2019. Please not that the program requires Administrative privileges to run due to the modification of HKLM registry keys.
+Download the latest version from the [Releases tab](https://github.com/Stormy102/WindowsRegistryWriteBlocker/releases). Alternatively you can download the repository and compile it from source with Visual Studio 2019. Please not that the program requires Administrative privileges to run due to the modification of HKLM registry keys.
 
 ## How it works
 
 The program works by modifying several Registry HKLM keys.
-> HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices
-> HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control
-These keys allow the user to prevent write access to USB devices.
+
+The most significant key modified is HKLM\SYSTEM\CurrentControlSet\Control\StorageDevicePolicies\WriteProtect, as this enables the Windows 
+The other keys modified enable the Group Policy found in Computer Configuration > Administrative Templates > System > Removable Storage Access > Removable Disks: Deny write access
+
+> HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}\Deny_Write to 1
+> HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\{53f56307-b6bf-11d0-94f2-00a0c91efb8b}\Deny_Write to 1
+> HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\Custom\Deny_Write\Deny_Write to 1
+> HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\Custom\Deny_Write\List\{53f5630d-b6bf-11d0-94f2-00a0c91efb8b} to {53f5630d-b6bf-11d0-94f2-00a0c91efb8b}
+> HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\Custom\Deny_Write\List\{53f56307-b6bf-11d0-94f2-00a0c91efb8b} to {53f56307-b6bf-11d0-94f2-00a0c91efb8b}
+
+> HKLM\SYSTEM\CurrentControlSet\Control\StorageDevicePolicies\WriteProtect to 1
+
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\DenyAllGPState to 1
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}\EnumerateDevices to 1
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}\AccessBitMask to 0
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}\UserPolicy to 0
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}\AuditPolicyOnly to 0
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}\SecurityDescriptor to "D:(D;;DCLCRPCRSD;;;IU)(A;;FA;;;SY)(A;;FA;;;LS)(A;;0x1200a9;;;IU)"
+
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}\EnumerateDevices to 1
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}\AccessBitMask to 0
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}\UserPolicy to 0
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}\AuditPolicyOnly to 0
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}\SecurityDescriptor to "D:(D;;DCLCRPCRSD;;;IU)(A;;FA;;;SY)(A;;FA;;;LS)(A;;0x1200a9;;;IU)"
+> HKLM\SYSTEM\CurrentControlSet\Control\Storage\HotplugSecurityDescriptor to the binary value 01000480000000000000000000000000140000000200580004000000010014001601010001010000000000050400000000001400ff011f0001010000000000051200000000001400ff011f0001010000000000051300000000001400a9001200010100000000000504000000
--- Windows Registry Write Blocker/Program.cs
+++ Windows Registry Write Blocker/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿/*
+ * Windows Write Blocker - a program that modifies registry keys to prevent writing to removable USB devices
+ * 
+ * LICENCE: MIT Licence
+ * 
+ */
+
+using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
+using System.Text;
 using System.Threading;
 using Microsoft.Win32;
 
 namespace WindowsRegistryWriteBlocker
 {
-    class Program
+    public class Program
     {
         private static bool IsWriteBlocked
         {
@@ -15,7 +24,7 @@ namespace WindowsRegistryWriteBlocker
                 using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\StorageDevicePolicies", RegistryKeyPermissionCheck.ReadWriteSubTree))
                 {
                     int writeProtect = (int)key.GetValue("WriteProtect");
-                    if (writeProtect == 1)
+                    if (writeProtect == 0)
                     {
                         return false;
                     }
@@ -49,7 +58,7 @@ namespace WindowsRegistryWriteBlocker
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("DISABLED");
+                Console.WriteLine("DISABLED/PARTIALLY ENABLED");
             }
             Console.ForegroundColor = ConsoleColor.White;
 
@@ -78,6 +87,7 @@ namespace WindowsRegistryWriteBlocker
                 Console.Write("> ");
 
                 var input = Console.ReadLine();
+                Console.Clear();
                 switch (input.ToLower())
                 {
                     case "e":
@@ -100,14 +110,78 @@ namespace WindowsRegistryWriteBlocker
                 key.SetValue("WriteProtect", enable ? 1 : 0);
             }
 
-            Console.WriteLine();
+            if (enable)
+            {
+                #region HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices
+
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\'{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}'", RegistryKeyPermissionCheck.ReadWriteSubTree))
+                {
+                    key.SetValue("Deny_Write", 1);
+                }
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\'{53f56307-b6bf-11d0-94f2-00a0c91efb8b}'", RegistryKeyPermissionCheck.ReadWriteSubTree))
+                {
+                    key.SetValue("Deny_Write", 1);
+                }
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\Custom\Deny_Write"))
+                {
+                    key.SetValue("Deny_Write", 1);
+                }
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\Custom\Deny_Write\List"))
+                {
+                    key.SetValue("{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}", "{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}");
+                    key.SetValue("{53f56307-b6bf-11d0-94f2-00a0c91efb8b}", "{53f56307-b6bf-11d0-94f2-00a0c91efb8b}");
+                }
+
+                #endregion
+
+                #region HKLM\SYSTEM\CurrentControlSet\Control\Storage\EnableDenyGP
+
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP"))
+                {
+                    key.SetValue("DenyAllGPState", 1);
+                }
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\'{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}'"))
+                {
+                    key.SetValue("EnumerateDevices", 1);
+                    key.SetValue("AccessBitMask", 0);
+                    key.SetValue("UserPolicy", 0);
+                    key.SetValue("AuditPolicyOnly", 0);
+                    key.SetValue("SecurityDescriptor", "D:(D;;DCLCRPCRSD;;;IU)(A;;FA;;;SY)(A;;FA;;;LS)(A;;0x1200a9;;;IU)");
+                }
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\'{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}'"))
+                {
+                    key.SetValue("EnumerateDevices", 1);
+                    key.SetValue("AccessBitMask", 0);
+                    key.SetValue("UserPolicy", 0);
+                    key.SetValue("AuditPolicyOnly", 0);
+                    key.SetValue("SecurityDescriptor", "D:(D;;DCLCRPCRSD;;;IU)(A;;FA;;;SY)(A;;FA;;;LS)(A;;0x1200a9;;;IU)");
+                }
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Storage"))
+                {
+                    key.SetValue("HotplugSecurityDescriptorTest", StringToByteArray("01000480000000000000000000000000140000000200580004000000010014001601010001010000000000050400000000001400ff011f0001010000000000051200000000001400ff011f0001010000000000051300000000001400a9001200010100000000000504000000"), RegistryValueKind.Binary);
+                }
+
+                #endregion
+            }
+            else
+            {
+
+                // Delete all of the Group Policy registry keys to revert write-blocking
+                Registry.LocalMachine.DeleteSubKeyTree(@"SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices", false);
+                Registry.LocalMachine.DeleteSubKey(@"SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices", false);
+                Registry.LocalMachine.DeleteSubKey(@"SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\'{53F5630D-B6BF-11D0-94F2-00A0C91EFB8B}'", false);
+                Registry.LocalMachine.DeleteSubKey(@"SYSTEM\CurrentControlSet\Control\Storage\EnabledDenyGP\'{53F56307-B6BF-11D0-94F2-00A0C91EFB8B}'", false);
+                Registry.LocalMachine.DeleteValue(@"SYSTEM\CurrentControlSet\Control\Storage\HotplugSecurityDescriptor", false);
+            }
 
             // Sleep to allow OS handles to update registry keys 
             Thread.Sleep(3000);
 
+            Console.WriteLine();
             if (enable)
             {
-                Console.WriteLine("Write blocking has been enabled. It is recommended to test with a non-evidential item first");
+                Console.WriteLine("Write blocking has been enabled");
+                Console.WriteLine("It is highly recommended to test with a non-evidential item first");
             }
             else
             {
@@ -116,9 +190,22 @@ namespace WindowsRegistryWriteBlocker
             Console.WriteLine();
         }
 
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
         private static bool CheckRequirements()
         {
             if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return false;
+            }
+
+            if (Environment.OSVersion.Version < new Version(5, 1))
             {
                 return false;
             }
